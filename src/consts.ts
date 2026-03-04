b13747d9528708ba3f107a2080dacc965cec15ae
Riverxia <riverdalecoder@outlook.com>
2026-03-04 10:25:39 -0500

    (acm) Currently Listening To:  -

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'idktheflag';
-export const SITE_DESCRIPTION = 'A CTF team focused on pwn, crypto, and web exploitation. Learn from our writeups and guides.';
--- src/content/docs/general/setup.md
+++
@@ -1,9 +0,0 @@
----
-title: Setup Guide
-description: How to not break the build.
----
-
-## Tools we use
-1. Bun
-2. Astro
-3. GDB (with Pwndbg)
